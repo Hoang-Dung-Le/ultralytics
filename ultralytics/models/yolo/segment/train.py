e0dc8c014b981414295f7bfbf62e0ebf6600f2e8
Le Hoang Dung <hoangdungle36@gmail.com>
2024-02-05 12:36:26 +0700

    + test img 31

--- ultralytics/models/yolo/segment/train.py
+++ ultralytics/models/yolo/segment/train.py
@@ -32,6 +32,7 @@ class SegmentationTrainer(yolo.detect.DetectionTrainer):
     def get_model(self, cfg=None, weights=None, verbose=True):
         """Return SegmentationModel initialized with specified config and weights."""
         model = SegmentationModel(cfg, ch=3, nc=self.data["nc"], verbose=verbose and RANK == -1)
+        print(model)
         if weights:
             model.load(weights)
 
